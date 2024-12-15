33b572009440d77ae179b8a4aec941f66102e994
Ethan Lew <15167713+ethanl21@users.noreply.github.com>
2024-12-14 18:30:30 -0800

    Implement student queries

--- query/student/course-history.php
+++ query/student/course-history.php
@@ -1,18 +1,33 @@
 <?php
 // Get the variables from the form's input(s)
-$cwid = $_GET["student-cwid"];
+$cwid = (int)$_GET["student-cwid"];
 
-// query the db here (TODO)
-$grades = [ // debug data
-	["CPSC 332", "A+"]
-];
+// query the db
+$mysqli = new mysqli("mariadb", "root", "O3YMMVEnGpR8kg2Iosq85", "temp_db"); // todo: change for campus server
+$stmt = $mysqli->prepare(
+	"
+SELECT `Student`.`CWID`, `Enrollment`.`COURSE_NO`, `Enrollment`.`GRADE`, `Course`.`TITLE`
+FROM `Student` 
+	LEFT JOIN `Enrollment` ON `Enrollment`.`CWID` = `Student`.`CWID` 
+	LEFT JOIN `Course` ON `Enrollment`.`COURSE_NO` = `Course`.`NUMBER`
+WHERE `Student`.`CWID` = ?;
+	"
+);
+$stmt->bind_param("i", $cwid);
+$stmt->execute();
 
-// generate the table rows
-foreach ($grades as $grade) {
-	echo '<tr>';
-	foreach ($grade as $cell) {
-		echo "<td>$cell</td>"; // Each cell
-	}
-	echo '</tr>';
+$result = $stmt->get_result();
+
+while ($row = $result->fetch_assoc()) {
+	$start_time = new DateTime($row["BEGIN_TIME"]);
+	$end_time = new DateTime($row["END_TIME"]);
+
+	// generate the table rows
+	echo "<tr>
+			<th scope=\"row\">" . $row["TITLE"] . "</th>
+			<td>" . $row["GRADE"] . "</td>
+			</tr>";
 }
-?>
+
+$stmt->close();
+$mysqli->close();
--- query/student/sections.php
+++ query/student/sections.php
@@ -1,18 +1,36 @@
 <?php
 // Get the variables from the form's input(s)
-$course_no = $_GET["student-course-no"];
+$course_no = (int)$_GET["student-course-no"];
 
-// query the db here (TODO)
-$sections = [ // debug data
-	[1, "CS 332", "MW 1:00-2:15PM", 99]
-];
+// query the db
+$mysqli = new mysqli("mariadb", "root", "O3YMMVEnGpR8kg2Iosq85", "temp_db"); // todo: change for campus server
+$stmt = $mysqli->prepare(
+	"
+SELECT `Course`.`NUMBER`, `Section`.`SECTION_NO`, `Section`.`CLASSROOM`, `Section`.`DAYS`, `Section`.`BEGIN_TIME`, `Section`.`END_TIME`, `Enrollment`.`CWID`, COUNT(*) as `ENROLLED`
+FROM `Course` 
+	LEFT JOIN `Section` ON `Section`.`COURSE_NO` = `Course`.`NUMBER` 
+	LEFT JOIN `Enrollment` ON `Enrollment`.`COURSE_NO` = `Course`.`NUMBER`
+WHERE `Course`.`NUMBER` = ?
+GROUP BY `NUMBER`, `SECTION_NO`;
+	"
+);
+$stmt->bind_param("i", $course_no);
+$stmt->execute();
 
-// generate the table rows
-foreach ($sections as $section) {
-	echo '<tr>';
-	foreach ($section as $cell) {
-		echo "<td>$cell</td>"; // Each cell
-	}
-	echo '</tr>';
+$result = $stmt->get_result();
+
+while ($row = $result->fetch_assoc()) {
+	$start_time = new DateTime($row["BEGIN_TIME"]);
+	$end_time = new DateTime($row["END_TIME"]);
+
+	// generate the table rows
+	echo "<tr>
+			<td>" . $row["SECTION_NO"] . "</td>
+			<td>" . $row["CLASSROOM"] . "</td>
+			<td>" . $row["DAYS"] . " " .  $start_time->format("h:i") . "-" . $end_time->format("h:i A") . "</td>
+			<td>" . $row["ENROLLED"] . "</td>
+			</tr>";
 }
-?>
+
+$stmt->close();
+$mysqli->close();
